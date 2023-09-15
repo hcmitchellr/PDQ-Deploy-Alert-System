3824d3f1914eff686b029c561e1378d326549cea
hcmitchellr <137416162+hcmitchellr@users.noreply.github.com>
2023-09-15 09:21:10 -0500

    Create ALERTS2.bat

---
+++ ALERTS2.bat
@@ -0,0 +1,37 @@
+<!-- :
+@echo off
+start "" mshta.exe "%~f0"
+exit /b
+-->
+<html>
+<head><title>YOUR TITLE</title>
+
+<style>
+body {
+  background-image: url("IMAGEPATH.IMG");
+  background-position: 100% 100%;
+  background-repeat: no-repeat;
+}
+</style>
+</head>
+<br><br><br>
+<body ><h1><font size="3" color="black" face="verdana">ENTER YOUR MAIN TEXT HERE . What is broken. Where is the Impact. How Many Users. 
+<br><br><br>
+Thank You,
+<br>
+IT Management
+</h1></font ></body>
+<br>
+  
+   <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> 
+   
+<button 
+
+onclick="myFunction()">CLOSE</button>
+
+<script>
+function myFunction() {
+    window.close();
+}
+</script>
+</html>
